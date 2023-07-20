776e919eb15072a8c628f88ec8fa305b8779e990
renatu <renatu_mb@yahoo.com.br>
2023-07-20 16:50:24 -0700

    - creation of hero-service - update heroes.component to use the hero-service - hero-service refactor to use Observable // https://angular.io/tutorial/tour-of-heroes/toh-pt4#create-the-heroservice

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Hero} from '../hero';
-import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +9,31 @@ import {HEROES} from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(
+        resp => {
+          this.heroes = resp
+        },
+        e => {
+          console.log('observable error heroService.getHeroes()' + e )
+        },
+        () => {
+          console.log('observable complete heroService.getHeroes()')
+        }
+      );
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
